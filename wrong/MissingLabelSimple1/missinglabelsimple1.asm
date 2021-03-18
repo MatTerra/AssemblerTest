6ac9982105a5185aa89ceadd6b6f32020ef25a27
MatTerra <mateus.b.s.terra@gmail.com>
2021-03-18 20:14:07 -0300

    First commit

---
+++ wrong/MissingLabelSimple1/missinglabelsimple1.asm
@@ -0,0 +1,7 @@
+            section data
+            section text
+start:      add     end                 ; Simple add something
+            jmp     ok
+            jmpn    ok
+; Let's finish this useless file
+end:        stop
---
+++ wrong/UnknownMnemonicSimple1/unknownmnemonicsimple1.asm
@@ -0,0 +1,7 @@
+            section data
+            section text
+start:                               ; Simple add something
+            jmp     end
+            jmpnz   end
+; Let's finish this useless file
+end:        stop

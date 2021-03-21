d5907baceb586cc3a8f95c9ab077774a181d8f19
MatTerra <mateus.b.s.terra@gmail.com>
2021-03-21 11:49:55 -0300

    more wrong codes

---
+++ wrong/RedeclaredLabelSimple1/redeclaredlabel1.asm
@@ -0,0 +1,8 @@
+section DATa
+just_single_data: cONSt 1
+section TExt
+adding_one: add just_single_data
+ADDING_one: add just_single_data
+;
+; This label was redefined
+stop
---
+++ wrong/WrongOperandCount/wrongoperandcount.asm
@@ -0,0 +1,7 @@
+section data
+space 1
+const
+section text
+start: mul start,start
+add
+stop

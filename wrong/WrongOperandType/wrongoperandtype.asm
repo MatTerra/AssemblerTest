64d97932db1249d0e772622020e50d112342266d
MatTerra <mateus.b.s.terra@gmail.com>
2021-03-31 09:00:44 -0300

    Added teacher example

--- wrong/WrongOperandType/wrongoperandtype.asm
+++ wrong/WrongOperandType/wrongoperandtype.asm
@@ -1,6 +1,10 @@
 section data
 one: const start;not allowed to use labels
+two: const 1.2
+three: const 2,0
 section text
 start:
 add 1 ; not allowed to use numbers
+output one+1 ; not allowed to sum
+output one + 1
 stop
